a0aaee564ad2e15942f9665bcc4db1aab37d9c5f
Tom Forbes <tom@tomforb.es>
2022-04-26 15:06:40 +0000

    {"name":"invent-with-python-zh","version":"2022.4.26.0","file":"invent_with_python_zh-2022.4.26.0-py3-none-any.whl","path":"invent-with-python-zh/invent_with_python_zh-2022.4.26.0-py3-none-any.whl"}

---
+++ invent-with-python-zh/invent_with_python_zh-2022.4.26.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""invent-with-python-zh
+https://github.com/apachecn/invent-with-python-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.4.26.0"

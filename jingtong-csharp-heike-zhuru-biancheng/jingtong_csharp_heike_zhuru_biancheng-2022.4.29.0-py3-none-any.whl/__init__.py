a1f7d2dc84ddc9b6a667d1233e400ba7f558746c
Tom Forbes <tom@tomforb.es>
2022-04-29 14:32:30 +0000

    {"name":"jingtong-csharp-heike-zhuru-biancheng","version":"2022.4.29.0","file":"jingtong_csharp_heike_zhuru_biancheng-2022.4.29.0-py3-none-any.whl","path":"jingtong-csharp-heike-zhuru-biancheng/jingtong_csharp_heike_zhuru_biancheng-2022.4.29.0-py3-none-any.whl"}

---
+++ jingtong-csharp-heike-zhuru-biancheng/jingtong_csharp_heike_zhuru_biancheng-2022.4.29.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingtong-csharp-heike-zhuru-biancheng
+https://github.com/apachecn/jingtong-csharp-heike-zhuru-biancheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.4.29.0"

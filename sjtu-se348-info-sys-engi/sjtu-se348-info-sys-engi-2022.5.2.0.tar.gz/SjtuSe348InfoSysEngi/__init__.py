44a55ae691eaaf06c8a6525b006035b58bc1d098
Tom Forbes <tom@tomforb.es>
2022-05-02 08:23:57 +0000

    {"name":"sjtu-se348-info-sys-engi","version":"2022.5.2.0","file":"sjtu-se348-info-sys-engi-2022.5.2.0.tar.gz","path":"sjtu-se348-info-sys-engi/sjtu-se348-info-sys-engi-2022.5.2.0.tar.gz"}

---
+++ sjtu-se348-info-sys-engi/sjtu-se348-info-sys-engi-2022.5.2.0.tar.gz/SjtuSe348InfoSysEngi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-se348-info-sys-engi
+https://github.com/apachecn/sjtu-se348-info-sys-engi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.2.0"

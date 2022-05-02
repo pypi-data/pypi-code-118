12ac70b2ba47c41e1f8637121c4477b24b7a0358
Tom Forbes <tom@tomforb.es>
2022-05-02 07:53:47 +0000

    {"name":"sjtu-se110-logic","version":"2022.5.2.0","file":"sjtu-se110-logic-2022.5.2.0.tar.gz","path":"sjtu-se110-logic/sjtu-se110-logic-2022.5.2.0.tar.gz"}

---
+++ sjtu-se110-logic/sjtu-se110-logic-2022.5.2.0.tar.gz/SjtuSe110Logic/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-se110-logic
+https://github.com/apachecn/sjtu-se110-logic"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.2.0"

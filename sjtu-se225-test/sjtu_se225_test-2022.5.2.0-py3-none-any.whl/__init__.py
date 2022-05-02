4abed72c9da1defec0baf1064ed03a141b449f07
Tom Forbes <tom@tomforb.es>
2022-05-02 08:03:01 +0000

    {"name":"sjtu-se225-test","version":"2022.5.2.0","file":"sjtu_se225_test-2022.5.2.0-py3-none-any.whl","path":"sjtu-se225-test/sjtu_se225_test-2022.5.2.0-py3-none-any.whl"}

---
+++ sjtu-se225-test/sjtu_se225_test-2022.5.2.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-se225-test
+https://github.com/apachecn/sjtu-se225-test"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.2.0"

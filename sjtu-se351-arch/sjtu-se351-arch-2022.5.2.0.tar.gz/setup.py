7be8bf6d0c68a25c6181993722270bfee8471b08
Tom Forbes <tom@tomforb.es>
2022-05-02 08:33:26 +0000

    {"name":"sjtu-se351-arch","version":"2022.5.2.0","file":"sjtu-se351-arch-2022.5.2.0.tar.gz","path":"sjtu-se351-arch/sjtu-se351-arch-2022.5.2.0.tar.gz"}

---
+++ sjtu-se351-arch/sjtu-se351-arch-2022.5.2.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import SjtuSe351Arch
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('SjtuSe351Arch'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="sjtu-se351-arch",
+    version=SjtuSe351Arch.__version__,
+    url="https://github.com/apachecn/sjtu-se351-arch",
+    author=SjtuSe351Arch.__author__,
+    author_email=SjtuSe351Arch.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="sjtu_se_351_软件架构",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "sjtu-se351-arch=SjtuSe351Arch.__main__:main",
+            "SjtuSe351Arch=SjtuSe351Arch.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)

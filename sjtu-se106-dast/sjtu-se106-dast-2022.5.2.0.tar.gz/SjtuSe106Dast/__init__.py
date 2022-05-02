e1e267fe34f1de54a50714cbc65af2295517ab77
Tom Forbes <tom@tomforb.es>
2022-05-02 07:50:54 +0000

    {"name":"sjtu-se106-dast","version":"2022.5.2.0","file":"sjtu-se106-dast-2022.5.2.0.tar.gz","path":"sjtu-se106-dast/sjtu-se106-dast-2022.5.2.0.tar.gz"}

---
+++ sjtu-se106-dast/sjtu-se106-dast-2022.5.2.0.tar.gz/SjtuSe106Dast/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-se106-dast
+https://github.com/apachecn/sjtu-se106-dast"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.2.0"

a9e3c4c727cd569e3669d7aeb05aec0de9685979
Tom Forbes <tom@tomforb.es>
2022-05-02 08:30:02 +0000

    {"name":"sjtu-se349-data-mining","version":"2022.5.2.0","file":"sjtu-se349-data-mining-2022.5.2.0.tar.gz","path":"sjtu-se349-data-mining/sjtu-se349-data-mining-2022.5.2.0.tar.gz"}

---
+++ sjtu-se349-data-mining/sjtu-se349-data-mining-2022.5.2.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import SjtuSe349DataMining
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('SjtuSe349DataMining'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="sjtu-se349-data-mining",
+    version=SjtuSe349DataMining.__version__,
+    url="https://github.com/apachecn/sjtu-se349-data-mining",
+    author=SjtuSe349DataMining.__author__,
+    author_email=SjtuSe349DataMining.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="sjtu_se349_数据仓库与数据挖掘",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "sjtu-se349-data-mining=SjtuSe349DataMining.__main__:main",
+            "SjtuSe349DataMining=SjtuSe349DataMining.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)

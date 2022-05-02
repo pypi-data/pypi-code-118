74d50bdcb4a736bafaa67a5b1f929ee4ecf3194c
Tom Forbes <tom@tomforb.es>
2022-05-02 08:15:56 +0000

    {"name":"sjtu-se332-info-sys-intro","version":"2022.5.2.0","file":"sjtu_se332_info_sys_intro-2022.5.2.0-py3-none-any.whl","path":"sjtu-se332-info-sys-intro/sjtu_se332_info_sys_intro-2022.5.2.0-py3-none-any.whl"}

---
+++ sjtu-se332-info-sys-intro/sjtu_se332_info_sys_intro-2022.5.2.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-se332-info-sys-intro
+https://github.com/apachecn/sjtu-se332-info-sys-intro"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.2.0"

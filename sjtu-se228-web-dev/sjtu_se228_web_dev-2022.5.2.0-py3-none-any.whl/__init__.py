7fe6a22b31901ad58c509a763c08e00761edb135
Tom Forbes <tom@tomforb.es>
2022-05-02 08:11:25 +0000

    {"name":"sjtu-se228-web-dev","version":"2022.5.2.0","file":"sjtu_se228_web_dev-2022.5.2.0-py3-none-any.whl","path":"sjtu-se228-web-dev/sjtu_se228_web_dev-2022.5.2.0-py3-none-any.whl"}

---
+++ sjtu-se228-web-dev/sjtu_se228_web_dev-2022.5.2.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-se228-web-dev
+https://github.com/apachecn/sjtu-se228-web-dev"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.2.0"

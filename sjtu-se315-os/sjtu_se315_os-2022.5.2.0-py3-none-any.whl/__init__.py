e228ba14299909b8136508809b7a3a724ef51103
Tom Forbes <tom@tomforb.es>
2022-05-02 08:14:12 +0000

    {"name":"sjtu-se315-os","version":"2022.5.2.0","file":"sjtu_se315_os-2022.5.2.0-py3-none-any.whl","path":"sjtu-se315-os/sjtu_se315_os-2022.5.2.0-py3-none-any.whl"}

---
+++ sjtu-se315-os/sjtu_se315_os-2022.5.2.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sjtu-se315-os
+https://github.com/apachecn/sjtu-se315-os"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.2.0"
